d05e7bda7cb0954f33c37da620cc42558a92dc1e
DucLQ92 <77601238+DucLQ92@users.noreply.github.com>
2025-05-28 14:36:31 +0700

    Home: add witch account button

--- src/main/HomeScreen.js
+++ src/main/HomeScreen.js
@@ -7,7 +7,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import type { RouteProp } from '../react-navigation';
 import type { MainTabsNavigationProp } from './MainTabsScreen';
-import { useDispatch } from '../react-redux';
+import { useGlobalDispatch, useGlobalSelector } from '../react-redux';
 import { HOME_NARROW, MENTIONED_NARROW, STARRED_NARROW } from '../utils/narrow';
 import { TopTabButton, TopTabButtonGeneral } from '../nav/TopTabButton';
 import UnreadCards from '../unread/UnreadCards';
@@ -19,6 +19,9 @@ import ServerCompatBanner from '../common/ServerCompatBanner';
 import ServerNotifsDisabledBanner from '../common/ServerNotifsDisabledBanner';
 import { OfflineNoticePlaceholder } from '../boot/OfflineNoticeProvider';
 import ServerNotifsExpiringBanner from '../common/ServerNotifsExpiringBanner';
+import { IconSwap } from '../common/Icons';
+import { getAccounts } from '../directSelectors';
+import { accountSwitch } from '../account/accountActions';
 
 const styles = createStyleSheet({
   wrapper: {
@@ -38,7 +41,21 @@ type Props = $ReadOnly<{|
 
 export default function HomeScreen(props: Props): Node {
   const { navigation } = props;
-  const dispatch = useDispatch();
+  const dispatch = useGlobalDispatch();
+  const accounts = useGlobalSelector(getAccounts);
+  const loggedInAccounts = accounts.filter(account => account.apiKey !== '');
+
+  const handleAccountSwitch = () => {
+    if (loggedInAccounts.length === 2) {
+      // Nếu có đúng 2 tài khoản đã đăng nhập, chuyển đổi trực tiếp
+      const currentAccount = loggedInAccounts[0];
+      const otherAccount = loggedInAccounts[1];
+      dispatch(accountSwitch({ realm: otherAccount.realm, email: otherAccount.email }));
+    } else {
+      // Nếu khác 2 tài khoản, mở màn hình AccountPick
+      navigation.push('account-pick');
+    }
+  };
 
   return (
     <SafeAreaView mode="padding" edges={['top']} style={styles.wrapper}>
@@ -69,6 +86,11 @@ export default function HomeScreen(props: Props): Node {
             navigation.push('search-messages');
           }}
         />
+        <TopTabButtonGeneral
+          onPress={handleAccountSwitch}
+        >
+          <IconSwap size={24} color={BRAND_COLOR} />
+        </TopTabButtonGeneral>
       </View>
       <ServerCompatBanner />
       <ServerNotifsDisabledBanner navigation={navigation} />
